95415820cc9357c09bd57c15b49e1edf58b364d6
John Gallagher <john.willis.gallagher@gmail.com>
2018-11-26 09:49:39 -0500

    Refacter code

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,6 @@ use objc::Encode;
 mod class;
 
 fn main() {
-    //test1();
-    //println!("--------- END OF TEST 1 ----------");
-    test2();
-}
-
-fn test2() {
     register_my_num();
 }
 
@@ -34,41 +28,6 @@ fn register_my_num() {
 }
 
 fn print_class_stats(cls: &objc::runtime::Class) {
-    println!("NSObject size: {}", cls.instance_size());
-
-    // Inspect its ivars
-    println!("NSObject ivars:");
-    for ivar in cls.instance_variables().iter() {
-        println!("{}", ivar.name());
-    }
-
-    // Allocate an instance
-    let obj = unsafe {
-        let obj: *mut Object = msg_send![cls, alloc];
-        let obj: *mut Object = msg_send![obj, init];
-        StrongPtr::new(obj)
-    };
-    println!("NSObject address: {:p}", obj);
-
-    // Access an ivar of the object
-    let isa: *const Class = unsafe { *(**obj).get_ivar("isa") };
-    println!("NSObject isa: {:?}", isa);
-
-    // Inspect a method of the class
-    let hash_sel = sel!(hash);
-    let hash_method = cls.instance_method(hash_sel).unwrap();
-    let hash_return = hash_method.return_type();
-    println!("-[NSObject hash] return type: {:?}", hash_return);
-    assert!(hash_return == usize::encode());
-
-    // Invoke a method on the object
-    let hash: usize = unsafe { msg_send![*obj, hash] };
-    println!("NSObject hash: {}", hash);
-}
-
-fn test1() {
-    // Get a class
-    let cls = class!(NSObject);
     println!("NSObject size: {}", cls.instance_size());
 
     // Inspect its ivars
